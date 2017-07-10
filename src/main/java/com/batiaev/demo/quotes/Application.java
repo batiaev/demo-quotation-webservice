4c9ca6e1dd498e2e5dc7de49e7dbb0afe552a6fe
Anton Batiaev <a.batyaev@gmail.com>
2017-07-11 00:13:12 +0300

    init project

---
+++ src/main/java/com/batiaev/demo/quotes/Application.java
@@ -0,0 +1,17 @@
+package com.batiaev.demo.quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author anton
+ * @since 10/07/17
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
