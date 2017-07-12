e84a228c477b9bf61b645663e1916a8ff3cee1f5
Anton Batiaev <a.batyaev@gmail.com>
2017-07-13 00:57:06 +0300

    add controller

---
+++ src/main/java/com/batiaev/demo/quotes/controller/RootController.java
@@ -0,0 +1,35 @@
+package com.batiaev.demo.quotes.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * RootController
+ *
+ * @author anton
+ * @since 13/07/17
+ */
+@RestController
+public class RootController {
+    private final Map<String, Object> appInfo = new HashMap<>();
+
+    @Value("${api.version:0.1.0}")
+    private String apiVersion;
+
+    @PostConstruct
+    public void init() {
+        appInfo.put("status", "ok");
+        appInfo.put("api.version", apiVersion);
+    }
+
+    @GetMapping(value = "/status")
+    public ResponseEntity<Map<String, Object>> status() {
+        return ResponseEntity.ok(appInfo);
+    }
+}
