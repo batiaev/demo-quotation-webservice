8093c719267d505349711c39ccc33815b3ec9c8a
Anton Batiaev <a.batyaev@gmail.com>
2017-07-13 01:25:46 +0300

    add swagger

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# demo-quotation-webservice
+# Demo: Spring RESTful quotation webservice
+
 Webinar https://geekbrains.ru/events/631
+
+## Build
+### Gradle
+gradle clean build
+
+### Maven
+mvn clean package
+
+## Run
+### Gradle
+gradle bootRun
+
+### Maven
+mvn spring-boot:run
+
+## Swagger
+http://localhost:9000/quotes/swagger-ui.html
+
+## Dependencies
+- junit
+- swagger
+- spring.boot
+
+## License
+[Apache-2.0](https://choosealicense.com/licenses/apache-2.0/)
+
+## Contacts
+- anton@batiaev.com
+- [batiaev.com](https://batiaev.com/)
--- src/main/java/com/batiaev/demo/quotes/Application.java
+++ src/main/java/com/batiaev/demo/quotes/Application.java
@@ -2,6 +2,14 @@ package com.batiaev.demo.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 /**
  * Application
@@ -10,8 +18,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 10/07/17
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(SWAGGER_2)
+                .groupName("quotes")
+                .apiInfo(apiInfo())
+                .select()
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Quotes REST API")
+                .contact(new Contact("Anton Batiaev",
+                        "https://github.com/Batiaev/demo-quotation-webservice",
+                        "anton@batiaev.com"))
+                .version("2.0")
+                .build();
     }
 }
